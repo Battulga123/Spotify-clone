6da0e04fbd2e49b80a68d983a73d1ee32f93bcb0
TulgaaDev <115762077+Battulga123@users.noreply.github.com>
2023-07-19 08:53:44 +0000

    from home

---
+++ spotify-clone/app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/Sidebar";
+
+
+export default function Home() {
+  return (
+    <div className='text-green-500'>
+
+    </div>
+  )
+}
+
---
+++ spotify-clone/components/Box.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface BoxProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Box: React.FC<BoxProps> = ({ children, className }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default Box;
